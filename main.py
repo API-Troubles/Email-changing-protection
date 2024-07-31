aed1da80048ab9fe7bd02173bad24c785de22722
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-31 20:07:43 +0000

    Work on getting POST data

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 from replit import db
-
 
 
 
@@ -10,10 +8,18 @@ app = Flask('app', template_folder="site_files/")
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/signup', methods = ['POST', 'GET'])
 def signup():
-  return "hi"
+    if request.method == 'POST':
+        print(request.form.get("password-field"))
+        return render_template("signup.html")
+
+    elif request.method == 'GET':
+        return render_template("signup.html")
+
+
 
 app.run(host='0.0.0.0', port=8080, debug=True)
