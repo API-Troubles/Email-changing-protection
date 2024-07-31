3b401621ded068fcfadd9f5a1c8724f37ac6b056
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-31 22:21:11 +0000

    Add login page, working on sessions

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import re
+import os
+
 from argon2 import PasswordHasher
-from flask import Flask, make_response, render_template, request, send_from_directory
+from flask import (
+    Flask,
+    make_response,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+    redirect,
+    session
+)
 from replit import db
 
 app = Flask(__name__, template_folder="site_files/")
 hasher = PasswordHasher()
+
+app.secret_key = os.environ['SECRET']
+
+email_check = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b')
 
 
 # Obtain static files dymanically
@@ -22,24 +38,41 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/signup', methods = ['POST', 'GET'])
+@app.route('/signup', methods = ['POST']) # type: ignore
 def signup():
-    if request.method == 'POST':
-        email = request.form.get("email-field")
-        password = request.form.get("password-field")
-        re_password = request.form.get("re-password-field")
+    email = request.form.get("email-field")
+    password = request.form.get("password-field")
+    re_password = request.form.get("re-password-field")
 
-        # Checks
-        if password is None or re_password is None:
-            return make_response("Invalid Data", 400)
-        if password != re_password:
-            return make_response("Invalid Data", 400)
+    # Checks
+    if password is None or re_password is None or email is None:
+        return make_response("Invalid Data", 400)
+    if password != re_password:
+        return make_response("Invalid Data", 400)
 
-        password_hashed = hasher.hash(password)
-        return render_template("signup.html")
+    if email_check.search(email) is None:
+        return make_response("Invalid Data", 400)
 
-    elif request.method == 'GET':
-        return render_template("signup.html")
+    db[email] = {
+        "password-hash": hasher.hash(password)
+    }
+    session['email'] = email
+    return redirect(url_for('settings'))
+
+
+@app.route('/login', methods = ['POST']) # type: ignore
+def login():
+    email = request.form.get("email-field")
+    password = request.form.get("password-field")
+
+    # Checks
+    if password is None or email is None:
+        return make_response("Invalid Data", 400)
+
+
+    if hasher.verify(db[email], password):
+        session['email'] = email
+        return redirect(url_for('settings'))
 
 
 
