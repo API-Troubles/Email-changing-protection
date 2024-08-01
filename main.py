73d0297d354f529d3cba3825e2e8fe693f0d6b5a
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-08-01 00:53:12 +0000

    Work on validation

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import re
 import os
+import re
 
 from argon2 import PasswordHasher, exceptions
 from flask import (
     Flask,
     make_response,
+    redirect,
     render_template,
     request,
     send_from_directory,
+    session,
     url_for,
-    redirect,
-    session
 )
 from replit import db
 
@@ -53,19 +53,19 @@ def signup():
         if email_check.search(email) is None:
             return make_response("Invalid Data", 400)
 
-        if db["email"] is not None:
-            return redirect(url_for('signup'))
+        if db.get(email) is not None:
+            return "email taken"
 
         db["email"] = {
             "password-hash": hasher.hash(password)
         }
         session['email'] = email
         return redirect(url_for('settings'))
-    else:
+    else: # Render signup page on GET req
         return render_template("signup.html")
 
 
-@app.route('/login', methods = ['POST']) # type: ignore
+@app.route('/login', methods = ['POST', 'GET']) # type: ignore
 def login():
     if request.method == "POST":
         email = request.form.get("email-field")
@@ -74,6 +74,9 @@ def login():
         # Checks
         if password is None or email is None:
             return make_response("Invalid Data", 400)
+
+        if db.get(email) is None:
+            return make_response("Email not found", 400)
 
         # Check if password is valid
         try:
@@ -86,6 +89,8 @@ def login():
             db[email]["password-hash"] = hasher.hash(password)
 
         return redirect(url_for("settings"))
+    else: # Render login page on GET req
+        return render_template("login.html")
 
 
 @app.route('/logout')
@@ -97,6 +102,11 @@ def logout():
 @app.route('/settings')
 def settings():
     return "settings"
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
