9f17bb8a330bee0932c39465586071267aca91f8
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-22 18:15:35 +0000

    Modify messaging, js additions

--- site_files/js/change_email.js
+++ site_files/js/change_email.js
@@ -1,12 +1,12 @@
 let submitBtn = null;
-let warningInfo = {};
+let warningInfo = {rushing: false, scam_info: false};
 
 function handleChange(event) {
     event.preventDefault();
     const errorBox = document.getElementById("error-msg");
 
     const urlParams = new URLSearchParams(window.location.search);
-    const email = urlParams.get("email-field"); // TODO CHANGE THIS
+    const email = urlParams.get("email-field");
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/change_email', true);
@@ -31,18 +31,27 @@ function handleChange(event) {
     xhr.send(JSON.stringify({ email: email }));
 }
 
+function warnWhyChange(btn) {
+    let selectedValue = document.querySelector('input[name=why-change-email]:checked')
+    let prompt = document.getElementById('warning-why');
+    if (['own-choice', 'trusted-family'].includes(selectedValue.value)) {
+        prompt.style.display = "none";
+        warningInfo.scam_info = false;
+    } else {
+        prompt.style.display = "block";
+        warningInfo.scam_info = true
+    }
+    changeBtnState();
+}
+
 function warnScams(btn) {
     let prompt = document.getElementById('warning-scams');
     if (btn.checked) { // This one is flipped from the rest, in this case yes = understand = gud
         prompt.style.display = "none";
-        if (warningInfo.scam_info) {
-            warningInfo.Remove("scam_info");
-        }
+        warningInfo.scam_info = false;
     } else {
         prompt.style.display = "block";
-        if (!warningInfo.scam_info) {
-            warningInfo.scam_info = ""
-        }
+        warningInfo.scam_info = true
     }
     changeBtnState();
 }
@@ -51,22 +60,23 @@ function warnRushing(btn) {
     let prompt = document.getElementById('warning-rush');
     if (btn.checked) { // Yes = rushing = bad
         prompt.style.display = "block";
-        warningScore++;
+        warningInfo.rushing = true;
     } else {
         prompt.style.display = "none";
-        warningScore--;
+        warningInfo.rushing = false;
     }
     changeBtnState();
 }
 
 function changeBtnState() {
     for (let w in warningInfo) {
-        if (w === true) {
-            submitBtn.disabled = false;
+        console.log(w)
+        if (warningInfo[w] === true) {
+            submitBtn.disabled = true;
             return
         }
     }
-    submitBtn.disabled = true;
+    submitBtn.disabled = false;
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -76,6 +86,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const urgentElements = document.getElementsByName('urgent-matter');
     const understandElements = document.getElementsByName('understand-scams');
+    const reasonElements = document.getElementsByName('why-change-email');
 
     urgentElements.forEach(radioBtn => {
         radioBtn.addEventListener('change', () => {warnRushing(urgentElements[0])});
@@ -83,5 +94,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     understandElements.forEach(radioBtn => {
         radioBtn.addEventListener('change', () => {warnScams(understandElements[0])});
+    });
+
+        reasonElements.forEach(radioBtn => {
+        radioBtn.addEventListener('change', () => {warnWhyChange(reasonElements)});
     });
 });
